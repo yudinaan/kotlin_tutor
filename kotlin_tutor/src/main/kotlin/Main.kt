12b6c76248b7088659b4682b9aa09abb16d778a6
yudinaan <yudinaanastasiya01@mail.ru>
2023-05-17 20:38:08 +0500

    workflow(): change state for checkout demonstration

--- kotlin_tutor/src/main/kotlin/Main.kt
+++ kotlin_tutor/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
     println("Hello World!")
     println("Hello, Nostuasia")
+    println("Hello again, Nastya")
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html
 }
